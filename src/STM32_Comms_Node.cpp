0583c490d621aef625ed7ebaa73610a5c0e17950
sumoPi <chrisherring1988@gmail.com>
2017-09-27 00:12:51 +1000

    Lots of changes, second night before competition

--- src/STM32_Comms_Node.cpp
+++ src/STM32_Comms_Node.cpp
@@ -4,6 +4,7 @@
 #include <wiringSerial.h>
 #include <ros/console.h>
 #include <std_msgs/UInt32MultiArray.h>
+#include <std_msgs/UInt16MultiArray.h>
 #include <std_msgs/UInt8.h>
 #include <geometry_msgs/Twist.h>
 #include <unistd.h>
@@ -35,14 +36,27 @@ private:
 	ros::NodeHandle nh;
 
 	// Wiring PI serial handle
-	std::string serialPort;
-	int hserial;
+	std::string serialPort_stm;
+	std::string serialPort_ard;
+	int hserial_stm;
+	int hserial_ard;
 
 	// Serial Comms Stuff
 	uint8_t newChar;
-	int RXDataLen;
-	int RXHeaderLen;
-	int RXFooterLen;
+
+	// For the STM32
+	int RXDataLen_stm;
+	int RXHeaderLen_stm;
+	int RXFooterLen_stm;
+	int RXLen_stm;
+	int RXStrState_stm;
+
+	// For the Arduino
+	int RXDataLen_ard;
+	int RXHeaderLen_ard;
+	int RXFooterLen_ard;
+	int RXLen_ard;
+	int RXStrState_ard;
 
 	int velTXbufferLen;
 	int velTXbufferHeaderLen;
@@ -55,10 +69,8 @@ private:
 	int32_t R_Motor;
 
 
-	int RXLen;
-	int RXStrState;
 	uint32_t sensorData[8];
-	uint8_t lineSensorData;
+	uint16_t lineSensorData[4];
 
 	// Pubs
 	ros::Publisher sensor_IR_Publisher;
@@ -72,19 +84,27 @@ private:
 
 void STM32_COMMS::InitialseVariables(void)
 {
-	// RX data length is eight uint32_t, eight ascii numbers identifying IT sensor data. Then an 'l' character and then the Line sensor state
-	RXDataLen = 4*8+8+2;
-	RXFooterLen = 3;	
-	RXHeaderLen = 5;	
-	RXLen = RXHeaderLen + RXDataLen + RXFooterLen;
-	RXStrState = 0;
-	lineSensorData = 0x00;
+	// STM RX data length is eight uint32_t, eight ascii numbers identifying IT sensor data. Then an 'l' character and then the Line sensor state
+	RXDataLen_stm = 4*8+8;
+	RXFooterLen_stm = 3;	
+	RXHeaderLen_stm = 5;	
+	RXLen_stm = RXHeaderLen_stm + RXDataLen_stm + RXFooterLen_stm;
+	RXStrState_stm = 0;
+
+	// Arduino data length is four uint32_t and four ascii numbers idnetifying them
+	RXDataLen_ard = 2*4+4;
+	RXHeaderLen_ard = 5;
+	RXFooterLen_ard = 3;
+	RXLen_ard = RXDataLen_ard + RXHeaderLen_ard + RXFooterLen_ard;
+	RXStrState_ard = 0;
+
 	maxPWMPulseWidth = 7999;
 	velTXbufferLen = 19;
 	velTXbufferHeaderLen = 6;
 	L_Motor = 0;
 	R_Motor = 0;
 	for (int i = 0; i < 8; i++) sensorData[i] = 0;
+	for (int i = 0; i < 4; i++) lineSensorData[i] = 0;
 }
 
 void STM32_COMMS::PublishIRSensor(void)
@@ -104,36 +124,51 @@ void STM32_COMMS::PublishIRSensor(void)
 }
 
 /**
-** Publish a Byte that contains the state of the line sensors
+** Publish analog readongs of the four line sensor data (NE, SE, SW, NW)
 **/
 void STM32_COMMS::PublishLineSensor(void)
 {
-	std_msgs::UInt8 data;
-	data.data = lineSensorData;
-	sensor_Line_Publisher.publish(data);
+	std_msgs::UInt16MultiArray array;
+
+	for (int i = 0; i < 4; i++)
+	{
+		array.data.push_back(lineSensorData[i]);
+	}
+	
+	sensor_Line_Publisher.publish(array);
 }
 
 STM32_COMMS::STM32_COMMS()
 {
 	// Get parameters
-	nh.param<std::string>("SerialPort", serialPort, "/dev/ttyACM0");
-	std::cout << "Using serial port: " << serialPort << std::endl;
+	nh.param<std::string>("serialPort_stm", serialPort_stm, "/dev/ttyACM0");
+	nh.param<std::string>("serialPort_ard", serialPort_ard, "/dev/ttyUSB0");
+	std::cout << "STM on serial port: " << serialPort_stm << std::endl;
+	std::cout << "Arduino on serial port: " << serialPort_ard << std::endl;
 
 	InitialseVariables();
 
 	// Attach advertisers
 	sensor_IR_Publisher 	= nh.advertise<std_msgs::UInt32MultiArray>("IR_sensor", 1);
-	sensor_Line_Publisher	= nh.advertise<std_msgs::UInt8>("Line_sensor", 1);
+	sensor_Line_Publisher	= nh.advertise<std_msgs::UInt16MultiArray>("Line_sensor", 1);
 
 	// -------- Set up wiringPi --------
 	wiringPiSetupSys();
-	if ( (hserial = serialOpen(serialPort.c_str(), 115200)) == -1)
+	if ( (hserial_stm = serialOpen(serialPort_stm.c_str(), 115200)) == -1)
 	{
-		printf("Failed to connect to device - Exiting\n");
+		printf("Failed to connect to STM device - Exiting\n");
 	}
 	else
 	{
-		std::cout << "Opened serial\n";
+		std::cout << "Opened STM serial\n";
+	}
+	if ( (hserial_ard = serialOpen(serialPort_ard.c_str(), 115200)) == -1)
+	{
+		printf("Failed to connect to Arduino device - Exiting\n");
+	}
+	else
+	{
+		std::cout << "Opened Arduino serial\n";
 	}
 
 	// Subscribe to topics
@@ -143,7 +178,8 @@ STM32_COMMS::STM32_COMMS()
 
 void STM32_COMMS::DeInit(void)
 {
-	serialClose(hserial);
+	serialClose(hserial_stm);
+	serialClose(hserial_ard);
 }
 
 /**
@@ -153,53 +189,90 @@ void STM32_COMMS::DeInit(void)
 **/
 void STM32_COMMS::RecieveData()
 {
-	while (serialDataAvail(hserial) > 0)
+	// Get data from stm
+	while (serialDataAvail(hserial_stm) > 0)
 	{
-		newChar = (uint8_t)serialGetchar(hserial);
+		newChar = (uint8_t)serialGetchar(hserial_stm);
 		
-		//std::cout << "-" << RXStrState << "-";
+		//std::cout << "-" << RXStrState_stm << "-";
 		//std::cout << newChar;
-		//if (RXStrState == 0) std::cout << std::endl;
+		//if (RXStrState_stm == 0) std::cout << std::endl;
 		// If we are still looking through header, make sure it matches the expected character, otherwise reset
-		if (RXStrState < RXHeaderLen)
+		if (RXStrState_stm < RXHeaderLen_stm)
 		{
-			(newChar == RXHeader[RXStrState]) ? RXStrState++ : RXStrState = 0;
+			(newChar == RXHeader[RXStrState_stm]) ? RXStrState_stm++ : RXStrState_stm = 0;
 		}
 		// If we are now looking at footer, make sure it matches the expected character, otherwise reset
-		else if (RXStrState >= RXHeaderLen + RXDataLen)
+		else if (RXStrState_stm >= RXHeaderLen_stm + RXDataLen_stm)
 		{
-			(newChar == RXFooter[RXStrState - RXHeaderLen - RXDataLen]) ? RXStrState++ : RXStrState = 0;
+			(newChar == RXFooter[RXStrState_stm - RXHeaderLen_stm - RXDataLen_stm]) ? RXStrState_stm++ : RXStrState_stm = 0;
 		}
 		// If this is the last databyte then it is the line sensor state
-		else if (RXStrState == RXHeaderLen + RXDataLen - 1)
-		{
-			lineSensorData = newChar;
-			RXStrState++;
-		}
+		//else if (RXStrState_stm == RXHeaderLen_stm + RXDataLen_stm - 1)
+		//{
+		//	//lineSensorData = newChar;
+		//	RXStrState_stm++;
+		//}
 		// If we are looking at the second last data bytes then make sure we get an 'l' followed by the line sensors byte
-		else if (RXStrState == RXHeaderLen + RXDataLen - 2)
-		{
-			(newChar == 'l') ? RXStrState++ : RXStrState = 0;
-		}
+		//else if (RXStrState_stm == RXHeaderLen_stm + RXDataLen_stm - 2)
+		//{
+		//	(newChar == 'l') ? RXStrState_stm++ : RXStrState_stm = 0;
+		//}
 		else
 		{
-			if ((RXStrState - RXHeaderLen) % 5 == 0)
-				((uint8_t)((RXStrState - RXHeaderLen) / 5) + '0' == newChar) ? RXStrState++ : RXStrState = 0;
+			if ((RXStrState_stm - RXHeaderLen_stm) % 5 == 0)
+				((uint8_t)((RXStrState_stm - RXHeaderLen_stm) / 5) + '0' == newChar) ? RXStrState_stm++ : RXStrState_stm = 0;
 			else
 			{
-				sensorData[(int)((RXStrState - RXHeaderLen) / 5)] |= newChar << 8*((RXStrState - RXHeaderLen) % 5-1);
-				RXStrState++;
+				sensorData[(int)((RXStrState_stm - RXHeaderLen_stm) / 5)] |= newChar << 8*((RXStrState_stm - RXHeaderLen_stm) % 5-1);
+				RXStrState_stm++;
 			}
 		}
-		if (RXStrState >= RXLen)
+		if (RXStrState_stm >= RXLen_stm)
 		{
 			PublishIRSensor();
-			PublishLineSensor();
 			for (int i = 0; i < 8; i++) sensorData[i] = 0;
-			RXStrState = 0;
+			RXStrState_stm = 0;
 			
 		}
 	}
+
+	// Get data from Arduino
+	while (serialDataAvail(hserial_ard) > 0)
+	{
+		newChar = (uint8_t)serialGetchar(hserial_ard);
+
+		// If we are still looking through header, make sure it matches the expected character, otherwise reset
+		if (RXStrState_ard < RXHeaderLen_ard)
+		{
+			(newChar == RXHeader[RXStrState_ard]) ? RXStrState_ard++ : RXStrState_ard = 0;
+		}
+		// If we are now looking at footer, make sure it matches the expected character, otherwise reset
+		else if (RXStrState_ard >= RXHeaderLen_ard + RXDataLen_ard)
+		{
+			(newChar == RXFooter[RXStrState_ard - RXHeaderLen_ard - RXDataLen_ard]) ? RXStrState_ard++ : RXStrState_ard = 0;
+		}
+		// Parse data byte
+		else
+		{
+			if ((RXStrState_ard - RXHeaderLen_ard) % 3 == 0)
+				((uint8_t)((RXStrState_ard - RXHeaderLen_ard) / 3) + '0' == newChar) ? RXStrState_ard++ : RXStrState_ard = 0;
+			else
+			{
+				lineSensorData[(int)((RXStrState_ard - RXHeaderLen_ard) / 3)] |= newChar << 8*((RXStrState_ard - RXHeaderLen_ard) % 3-1);
+				RXStrState_ard++;
+			}
+		}
+
+		if (RXStrState_ard >= RXLen_ard)
+		{
+			PublishLineSensor();
+			for (int i = 0; i < 4; i++) lineSensorData[i] = 0;
+			RXStrState_ard = 0;
+		}
+
+	}
+
 }
 
 
@@ -211,8 +284,10 @@ void STM32_COMMS::RecieveData()
 **/
 void STM32_COMMS::set_vel_callback(const geometry_msgs::Twist msg)
 {
-	L_Motor = (int32_t)(msg.linear.x*maxPWMPulseWidth);
-	R_Motor = (int32_t)(msg.linear.y*maxPWMPulseWidth);
+	L_Motor =  (int32_t)(msg.linear.x*maxPWMPulseWidth);
+	// Invert the right motor velocity now.
+	R_Motor = -(int32_t)(msg.linear.y*maxPWMPulseWidth);
+	setVelocities();
 }
 
 /**
@@ -226,7 +301,7 @@ void STM32_COMMS::setVelocities()
 	//R_Motor += 100;
 	//L_Motor %= 7999;
 	//R_Motor %= 7999;
-	serialFlush(hserial);
+	serialFlush(hserial_stm);
 	//memcpy(TXbuffer, "start!LaaaaRaaaaend", 19);
 	memcpy(TXbuffer, "start!", 6);
 	memcpy(TXbuffer+velTXbufferLen-3, "end", 3);
@@ -241,12 +316,12 @@ void STM32_COMMS::setVelocities()
 		TXbuffer[12+i] = (uint8_t)((R_Motor >> 8*i)&0xFF);
 	}
 	//printf("L = %ld, R = %ld, %s\n", L_Motor, R_Motor, TXbuffer);
-	//serialFlush(hserial);
+	//serialFlush(hserial_stm);
 	for (int i = 0; i < 19; i++)
 	{
-		serialPutchar (hserial, (unsigned char)TXbuffer[i]) ;
+		serialPutchar (hserial_stm, (unsigned char)TXbuffer[i]) ;
 	}
-	//write(hserial, (const char*)TXbuffer, 19);
+	//write(hserial_stm, (const char*)TXbuffer, 19);
 
 }
 
@@ -267,7 +342,7 @@ int main(int argc, char** argv)
 	{
 		// Check if there is data to recieve. If there is then recieve it and parse it
 		STM32_Comms.RecieveData();
-		STM32_Comms.setVelocities();
+		//STM32_Comms.setVelocities();
 
 		// Spin then sleep
 		ros::spinOnce();
